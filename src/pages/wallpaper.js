4fb25a1fd6de99f137cf295d0ecbf164c791f958
NaveenNaiduSetti <85381717+SettiNaveenNaidu@users.noreply.github.com>
2023-08-14 17:27:13 +0530

    Add files via upload

---
+++ src/pages/wallpaper.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Large=()=>{
+    const token=localStorage.getItem('jwtToken')
+    console.log(token)
+    return(
+        <div className="large_image">
+            <div className="large_text">
+            <div>Stay Curiues</div>
+            <div style={{fontSize:'20px'}}>Discover stories, thinking, and expertise from writers on any topic.</div>
+            <button className="start_reading">Start Reading</button>
+            </div>
+        </div>
+    )
+}
+export default Large
